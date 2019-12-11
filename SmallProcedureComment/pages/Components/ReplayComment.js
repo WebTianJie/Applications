89a6eb00318048413bb067421cb61f290e6b8a10
天劫天罪 <993695313@qq.conm>
2019-12-11 10:18:29 +0800

    首页展示,回复信息

--- SmallProcedureComment/pages/Components/ReplayComment.js
+++ SmallProcedureComment/pages/Components/ReplayComment.js
@@ -1,4 +1,5 @@
 // pages/Components/ReplayComment.js
+const app=getApp();
 Component({
   /**
    * 组件的属性列表
@@ -20,6 +21,14 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    addFabulousReplay(e){
+        var  param={
+       isClicked : e.currentTarget.dataset.isclicked,
+       count : parseInt(e.currentTarget.dataset.fabulous),
+       id : parseInt(e.currentTarget.dataset.id)
+    }
+    this.triggerEvent('addFabulousReplay',param);
     
+    }
   }
 })
